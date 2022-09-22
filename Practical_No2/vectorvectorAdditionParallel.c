61f93f02f6a03dd066b1bc064f6ab0687289567c
“OnkarGavali” <onkargavali1561@gmail.com>
2022-09-23 00:30:57 +0530

    practical 2 added

---
+++ Practical_No2/vectorvectorAdditionParallel.c
@@ -0,0 +1,27 @@
+#include <omp.h>
+#include <stdio.h>
+#include <pthread.h>
+void main()
+{
+int N = 10000;
+int A[10000];
+for(int i=0;i<N;i++)A[i] = i + 1;
+int B[10000];
+for(int i=0;i<N;i++)B[i] = N - i;
+int C[10000] = {0};
+double itime, ftime, exec_time;
+itime = omp_get_wtime();
+#pragma omp parallel for reduction(+ : C)
+for (int i = 0; i < N; i++)
+{
+C[i] = A[i] + B[i];
+printf("Thread: %d Index: %d\n", omp_get_thread_num(),i);
+}
+for(int i=0;i<N;i++){
+printf("%d ", C[i]);
+}
+ftime = omp_get_wtime();
+exec_time = ftime - itime;
+printf("\nTime taken is %f\n", exec_time);
+printf("\n");
+}
